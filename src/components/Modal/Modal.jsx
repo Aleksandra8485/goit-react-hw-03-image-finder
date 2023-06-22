74cfcf948aeb7396251113fd56e821fd070e72da
Aleksandra <a.walczak1984@gmail.com>
2023-06-22 12:28:38 +0200

    added basiclightbox

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 const Modal = ({ imageUrl, alt, onClose }) => {
   useEffect(() => {
@@ -14,13 +16,44 @@ const Modal = ({ imageUrl, alt, onClose }) => {
     };
   }, [onClose]);
 
+  const openLightbox = () => {
+    basicLightbox.create(`<img src="${imageUrl}" alt="${alt}" />`).show();
+  };
+
   return (
     <div className="overlay" onClick={onClose}>
       <div className="modal">
-        <img src={imageUrl} alt={alt} />
+        <img src={imageUrl} alt={alt} onClick={openLightbox} />
       </div>
     </div>
   );
 };
 
 export default Modal;
+
+// import React, { useEffect } from 'react';
+
+// const Modal = ({ imageUrl, alt, onClose }) => {
+//   useEffect(() => {
+//     const handleKeyDown = event => {
+//       if (event.key === 'Escape') {
+//         onClose();
+//       }
+//     };
+
+//     document.addEventListener('keydown', handleKeyDown);
+//     return () => {
+//       document.removeEventListener('keydown', handleKeyDown);
+//     };
+//   }, [onClose]);
+
+//   return (
+//     <div className="overlay" onClick={onClose}>
+//       <div className="modal">
+//         <img src={imageUrl} alt={alt} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Modal;
